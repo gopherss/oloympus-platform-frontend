b6e667fc4341bee00afb7f8e0a149dd0e16baae2
SANDRO CASTRO PAREDES <sandrogopher@gmail.com>
2024-06-09 00:37:20 -0500

    Primera versión Olympus Project archivos despliegue

--- src/components/pages/RegisterPage.tsx
+++ src/components/pages/RegisterPage.tsx
@@ -11,7 +11,7 @@ const RegisterPage: React.FC = () => {
                     <div className="sm:col-span-3">
                         <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">First name</label>
                         <div className="mt-2">
-                            <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                            <input autoFocus type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
                         </div>
                     </div>
 
